1961f0c8be90bf85cd90adc006997eabae322f9e
coco483 <151052125+coco483@users.noreply.github.com>
2024-03-07 20:20:37 +0900

    bug fix and added printHangman() function

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ MoonJung Kim
 lunatic2959@gmail.com
 
 simple hangman game wrote by c++
+
+found bugs:
+ - 첫 시도에 무조건 Please enter an alphabet이 뜬다
+
+future implementation 
+ - game over image
+ - randomWord() implementation
+
+fixed bugs
+ - 이미 입력한 알파벳을 다시 입력할 때 success로 처리됨 
+ - when multiple characters are typed together, get the first character and flush the rest
--- hangman.cpp
+++ hangman.cpp
@@ -3,11 +3,14 @@
 #include <string>
 #include <set>
 
-#define LIVES 5;
+#define MAX_LIVES 7;
 
-using std::cout, std::cin, std::string, std::endl, std::set;
+using std::cout; using std::cin; using std::endl;
+using std::string; using std::set;
+
 void startGame();
 string randomWord();
+void printHangman(int);
 
 int main(void){
     //using namespace std;
@@ -19,15 +22,15 @@ int main(void){
         else if (ans == 'n') break;
     }
 }
-
 void startGame(void){
     cout << "let's play!" << endl;
     string word = randomWord();
-    int lives = LIVES;
+    int lives = MAX_LIVES;
     set<char> crossedOut;
     char ch;
     while (true){
         cout <<endl;
+        printHangman(lives);
         int charLeft = 0;
         for (auto i :word){
             if (i == ' ') cout << ' ';
@@ -38,15 +41,16 @@ void startGame(void){
             }
         }
         if (charLeft == 0){
-            cout << endl << "You guessed the word! congrats!" <<endl;
+            cout << "\n You guessed the word! congrats!" <<endl;
             break;
         }
         cout << endl << "Enter an alphabet: ";
-        cin >> ch;
+        ch = getchar();
+        while(getchar() != '\n'){}
         if (!isalpha(ch)){
             cout << "Please enter an alphabet" <<endl;
             continue;
-        } else if (crossedOut.count(ch)) {
+        } else if (crossedOut.count(ch) == 1) {
             cout << "You already tried this alphabet" <<endl;
             continue;
         }
@@ -60,6 +64,23 @@ void startGame(void){
             cout << "failed! " << lives << " lives left!" << endl;
         }
     }
+}
+
+void printHangman(int lives)
+{
+    string hangmanImage = //9*7
+        "   +---+\n"
+        "   |   |\n"
+        "   O   |\n"
+        "  /|\\  |\n"
+        "  / \\  |\n"
+        "       |\n"
+        "========\n";
+    int positions[] = {40, 38, 31, 30, 29, 21};
+    for (int i=0; i <lives-1; i++){
+        hangmanImage[positions[i]] = ' ';
+    }
+    cout<<hangmanImage<<endl;
 }
 
 string randomWord(){
